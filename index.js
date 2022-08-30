62478ce1351c8a12179b75b584e26420c5d7daa7
Joshua Campos <jdeveloper1203@gmail.com>
2022-08-30 15:54:11 -0400

    commit tree 88ca810d9d22c89a3047fa561902d67cb299da74

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+
+function superbowlWin(array){
+
+    // for ([key, values] of Object.entries(array)){
+    //     return 
+    // }
+    
+    // console.log(Object.entries(array))
+    let wins = array.find(r => {
+        if(r.result === "W"){
+            return r.year;
+        } 
+    });
+
+    if (wins){
+        return wins.year;
+    } else{
+        return undefined;
+    }
+};
